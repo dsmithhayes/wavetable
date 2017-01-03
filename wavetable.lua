c2092cfa095eeeb26001d1d7152e81b3f1a7b6a4
Dave Smith-Hayes <dsmithhayes@gmail.com>
2017-01-02 23:11:56 -0500

    More familiar for other Lua developers.

--- wavetable.lua
+++ wavetable.lua
@@ -4,62 +4,89 @@
   @Author:  Dave Smith-Hayes <me@davesmithhayes.com>
 --]]--
 local wavetable = {}
+wavetable.__index = wavetable
 
-local sample_rate = 44100
-local table_size  = 1024
-local f_freq      = 1 / (table_size / sample_rate)
-local two_pi      = 2 * math.pi
-local two_by_pi   = 2 / math.pi
+--[[--
+  Constants
+--]]--
+local TWO_PI    = 2 * math.pi
+local TWO_BY_PI = 2 / math.pi
+
+--[[--
+   Call `wavetable` as a constructor.
+--]]--
+setmetatable(wavetable, {
+  __call = function (cls, ...)
+    return cls.new(..., ...)
+  end
+})
+
+--[[--
+  The main constructor for the wavetable object.
+
+  @Parameter: number
+    The sample rate to set for the wavetable
+  @Parameter: number
+    The number of samples to set for the wavetable
+--]]--
+function wavetable:new(init_sr, init_s)
+  local self = setmetatable({}, wavetable)
+  self.sample_rate = init_sr
+  self.samples = init_s
+  self.freq = 1 / (self.samples / self.sample_rate)
+
+  return self
+end
 
 --[[--
   @Return: number
     The sample rate set
 --]]--
-function wavetable.get_sample_rate()
-  return sample_rate
+function wavetable:get_sample_rate()
+  return self.sample_rate
 end
 
 --[[--
   @Param: number
     The new sample rate
 --]]--
-function wavetable.set_sample_rate(sr)
-  f_freq = wavetable.gen_f_freq(sr, table_size)
-  sample_rate = sr
+function wavetable:set_sample_rate(sr)
+  self.sample_rate = sr
+  self.freq = wavetable:gen_freq(self.sample_rate, self.samples)
 end
 
 --[[--
   @Return: number
     The number of samples in the wavetable
-]]
-function wavetable.get_table_size()
-  return table_size
+--]]--
+function wavetable:get_samples()
+  return self.samples
 end
 
 --[[--
   @Parameter: number
     The new number of samples in the wavetable
 --]]--
-function wavetable.set_table_size(ts)
-  f_freq = wavetable.gen_f_freq(sample_rate, ts)
-  table_size = ts
+function wavetable:set_samples(s)
+  self.samples = s
+  self.freq = wavetable:gen_freq(self.sample_rate, self.samples)
 end
 
 --[[--
   @Parameter: number
     The fundamental frequency to use
 --]]--
-function wavetable.set_f_freq(f)
-  table_size = wavetable.gen_table_size(sample_rate, f)
-  f_freq = f
+function wavetable:set_freq(f)
+  self.freq = f
+  self.samples = wavetable:gen_samples(self.sample_rate, self.freq)
 end
 
 --[[--
   @Return: number
     The fundamental frequency
 --]]--
-function wavetable.get_f_freq()
-  return f_freq
+function wavetable:get_freq()
+  return self.freq
 end
 
 --[[--
@@ -70,8 +97,8 @@ end
   @Return: number
     The fundamental frequency
 --]]--
-function wavetable.gen_f_freq(sr, ts)
-  return 1 / (ts / sr)
+function wavetable:gen_freq(sr, s)
+  return 1 / (s / sr)
 end
 
 --[[--
@@ -82,7 +109,7 @@ end
   @Return: number
     The table size for the frequency and sample rate
 --]]--
-function wavetable.gen_table_size(sr, f)
+function wavetable:gen_samples(sr, f)
   return sr * (1 / f)
 end
 
@@ -94,8 +121,8 @@ end
   @Return: number
     The phase increment
 --]]--
-function wavetable.gen_phase_inc(sr, f)
-  return (two_pi / sr) * f
+function wavetable:gen_phase_inc(sr, f)
+  return (TWO_PI / sr) * f
 end
 
 --[[--
@@ -106,7 +133,7 @@ end
   @Return: number
     The saw increment
 --]]--
-function wavetable.gen_saw_inc(sr, f)
+function wavetable:gen_saw_inc(sr, f)
   return (2 * f) / sr
 end
 
@@ -116,23 +143,23 @@ end
   @Return: table
     Table of all the values in the waveform
 --]]--
-function wavetable.gen_sin(f)
-  local ts = table_size
+function wavetable:gen_sin(f)
+  local s = self.samples
   if f then
-    ts = wavetable.gen_table_size(sample_rate, f)
+    s = wavetable:gen_samples(self.sample_rate, f)
   end
 
-  local freq       = f or f_freq
-  local phase_inc  = wavetable.gen_phase_inc(sample_rate, freq)
+  local freq       = f or self.freq
+  local phase_inc  = wavetable:gen_phase_inc(self.sample_rate, freq)
   local phase      = 0
   local wave_table = {}
 
-  for i = 1, ts do
+  for i = 1, s do
     wave_table[i] = math.sin(phase)
     phase = phase + phase_inc
 
-    if phase >= two_pi then
-      phase = phase - two_pi
+    if phase >= TWO_PI then
+      phase = phase - TWO_PI
     end
   end
 
@@ -148,24 +175,24 @@ end
   @Return: table
     The generated wavetable
 --]]--
-function wavetable.gen_sqr(f, dc)
-  local ts = table_size
+function wavetable:gen_sqr(f, dc)
+  local s = self.samples
   if f then
-    ts = wavetable.gen_table_size(sample_rate, f)
+    s = wavetable:gen_samples(self.sample_rate, f)
   end
 
-  local freq       = f or f_freq
+  local freq       = f or self.freq
   local duty       = dc or 50
-  local mid_point  = two_pi * (duty / 100)
-  local phase_inc  = wavetable.gen_phase_inc(sample_rate, freq)
+  local mid_point  = TWO_PI * (duty / 100)
+  local phase_inc  = wavetable:gen_phase_inc(self.sample_rate, freq)
   local phase      = 0
   local wave_table = {}
 
-  for i = 1, ts do
+  for i = 1, s do
     phase = phase + phase_inc
 
-    if phase >= two_pi then
-      phase = phase - two_pi
+    if phase >= TWO_PI then
+      phase = phase - TWO_PI
     end
 
     if phase >= mid_point then
@@ -184,18 +211,18 @@ end
   @Return: table
     The generated wavetable
 --]]--
-function wavetable.gen_saw(f)
-  local ts = table_size
+function wavetable:gen_saw(f)
+  local s = self.samples
   if f then
-    ts = wavetable.gen_table_size(sample_rate, f)
+    s = wavetable:gen_table_size(self.sample_rate, f)
   end
 
-  local freq       = f or f_freq
-  local saw_inc    = wavetable.gen_saw_inc(sample_rate, freq)
+  local freq       = f or self.freq
+  local saw_inc    = wavetable:gen_saw_inc(self.sample_rate, freq)
   local saw_val    = -1
   local wave_table = {}
 
-  for i = 1, ts do
+  for i = 1, s do
     wave_table[i] = saw_val
     saw_val = saw_val + saw_inc
 
@@ -213,19 +240,19 @@ end
   @Return: table
     The generated wavetable
 --]]--
-function wavetable.gen_tri(f)
-  local ts = table_size
+function wavetable:gen_tri(f)
+  local s = self.samples
   if f then
-    ts = wavetable.gen_table_size(sample_rate, f)
+    s = wavetable:gen_samples(self.sample_rate, f)
   end
 
-  local freq       = f or f_freq
+  local freq       = f or self.freq
   local wave_table = {}
-  local phase_inc  = wavetable.gen_phase_inc(sample_rate, freq)
+  local phase_inc  = wavetable:gen_phase_inc(self.sample_rate, freq)
   local phase      = 0
 
-  for i = 1, ts do
-    local tri_val = phase * two_by_pi
+  for i = 1, s do
+    local tri_val = phase * TWO_BY_PI
 
     if tri_val < 0 then
       tri_val = 1.0 + tri_val
@@ -237,7 +264,7 @@ function wavetable.gen_tri(f)
     phase = phase + phase_inc
 
     if phase >= math.pi then
-      phase = phase - two_pi
+      phase = phase - TWO_PI
     end
   end
 
